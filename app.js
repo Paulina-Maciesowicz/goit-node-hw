c8f60df58a99a75831acc9d1a87b40e4a395f726
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-08-19 22:20:57 +0200

    update v1
    update v1

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
 const contactsRouter = require('./routes/api/contacts')
-
 const app = express()
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const contactRoutes = require("./routes/contacts.routes");
+const authRoutes = require("./routes/auth.routes");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+const connection = mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
-app.use('/api/contacts', contactsRouter)
-
+app.use("/api/v1", contactRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/contacts", contactsRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
-
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
+
+  connection
+    .then(() => {
+      console.log("Database connection successful");
+      app.listen(PORT, () => {
+        console.log(`App listens on port ${PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error(`Error while establishing connection: [${err}]`);
+      process.exit(1);
+    });
 })
 
 module.exports = app
+
+
+
